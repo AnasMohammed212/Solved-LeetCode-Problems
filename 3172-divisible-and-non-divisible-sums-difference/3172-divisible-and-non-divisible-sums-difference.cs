a4d899086a1bbe118c27c16b3bc711e7d3218fc5
AnasMohammed212 <110040495+AnasMohammed212@users.noreply.github.com>
2024-10-08 23:22:08 +0300

    Time: 32 ms (6.07%), Space: 27 MB (75%) - LeetHub

---
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int DifferenceOfSums(int n, int m) {
+        int DivisibleNumbers = 0, NonDivisibleNumbers = 0;
+for (int i = 1; i <= n; i++) {
+    if (i % m == 0)
+        DivisibleNumbers+=i;
+    else
+        NonDivisibleNumbers += i;
+}
+return NonDivisibleNumbers- DivisibleNumbers;
+    }
+}
